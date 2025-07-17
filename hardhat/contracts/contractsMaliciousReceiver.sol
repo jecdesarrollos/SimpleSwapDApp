aff4cb78c7dc4207f103c725e2e1635fb3c57cc4
jecdesarrollos <120194232+jecdesarrollos@users.noreply.github.com>
2025-07-17 02:38:13 -0300

    SimpleSwapDApp update

--- hardhat/contracts/contractsMaliciousReceiver.sol
+++ hardhat/contracts/contractsMaliciousReceiver.sol
@@ -8,15 +8,11 @@ contract MaliciousReceiver {
     }
 
     function executeWithdraw(address simpleSwapAddress) external {
-        // Hacemos la llamada y esta vez SÍ capturamos los valores de retorno.
         (bool success, bytes memory returnData) = simpleSwapAddress.call(
             abi.encodeWithSignature("withdrawETH()")
         );
 
-        // Si la llamada interna no tuvo éxito...
         if (!success) {
-            // ...revertimos, pero pasamos la información del error original (`returnData`).
-            // Esto asegura que el error de `SimpleSwap` "burbujee" hasta el test.
             assembly {
                 revert(add(returnData, 32), mload(returnData))
             }
